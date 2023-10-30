4db0b1e9372375320e9a051fc2799dcfe3db180f
Toby Nott <tobynott80@gmail.com>
2023-10-30 18:17:30 +0000

    added menu icon

---
+++ app/components/icons/icons.js
@@ -0,0 +1,19 @@
+const MenuIcon = () => (
+  <svg
+    className="w-6 h-6 text-gray-800 dark:text-white"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 17 14"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M1 1h15M1 7h15M1 13h15"
+    />
+  </svg>
+);
+
+export default MenuIcon;
