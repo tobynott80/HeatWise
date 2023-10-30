2165300a23599e292d1396e01f2790ab58ca8479
Toby Nott <tobynott80@gmail.com>
2023-10-30 19:02:47 +0000

    added icons for menu

--- app/components/icons/Menu.js
+++ app/components/icons/Menu.js
@@ -1,10 +1,12 @@
+// Icons free and open source from https://flowbite.com/icons/
+
 const MenuIcon = () => (
   <svg
-    className="w-6 h-6 text-gray-800 dark:text-white"
+    className="w-4 h-4 text-gray-800 dark:text-white"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    viewBox="0 0 17 14"
+    viewBox="0 0 20 14"
   >
     <path
       stroke="currentColor"
