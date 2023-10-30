992ae2db0f01da073a6f38e365106b6944834415
Toby Nott <tobynott80@gmail.com>
2023-10-30 19:03:30 +0000

    removed unnecessary padding

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import NavContainer from "./components/NavContainer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
       <NavContainer />
     </main>
   )
