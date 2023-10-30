92556632f94319d932385c69a21bbf513c97da0e
Toby Nott <tobynott80@gmail.com>
2023-10-30 23:04:27 +0000

    added collapse functionality

--- app/components/NavContainer.js
+++ app/components/NavContainer.js
@@ -1,71 +1,87 @@
+"use client";
 import Link from "next/link";
 import MenuIcon from "./icons/Menu";
 import GlobeIcon from "./icons/Globe";
 import HomeIcon from "./icons/Home";
 import PoundIcon from "./icons/Pound";
 import GraphDown from "./icons/GraphDown";
+import { useState } from "react";
 
 const MenuItem = ({ title, href, icon, isActive }) => {
-  const classes = `flex items-center px-6 py-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white ${
+  const classes = `flex items-center py-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white ${
     isActive ? "border-2 rounded-lg border-gray-200 dark:border-gray-700" : ""
   }`;
   return (
     <Link className={classes} href={href}>
-      {icon}
-      <span className="mx-2 font-medium">{title}</span>
+      <div className="ml-3">{icon}</div>
+      <span className="mx-4 font-medium">{title}</span>
     </Link>
   );
 };
 
 export default function NavContainer({ activeLink }) {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  const sidebarWidth = isSidebarOpen ? "w-[300px]" : "w-[75px]";
+
   return (
-    <div className="fixed left-0 h-screen w-[300px] flex flex-col bg-white dark:bg-gray-800 overflow-y-auto">
-      <div className="flex items-center justify-start h-16 px-6 border-b">
-        <MenuIcon />
-        <span className="ml-2 text-lg font-semibold text-gray-700 dark:text-gray-200">
-          HeatWise
-        </span>
+    <div
+      className={`fixed left-0 h-screen ${sidebarWidth} flex flex-col bg-white dark:bg-gray-800 overflow-y-auto`}
+    >
+      <div className="flex items-center justify-between h-16 px-6 border-b">
+        <button className="ml-2" onClick={toggleSidebar}>
+          <MenuIcon />
+        </button>
+        {isSidebarOpen && (
+          <span className="ml-2 text-lg font-semibold text-gray-700 dark:text-gray-200">
+            HeatWise
+          </span>
+        )}
       </div>
-      <nav className="flex flex-col py-4 px-2 flex-grow">
+      <nav
+        className={`flex flex-col py-4 px-4 ${
+          isSidebarOpen ? "flex-grow" : ""
+        }`}
+      >
         <MenuItem
-          title="Home"
+          title={isSidebarOpen ? "Home" : ""}
           href="/"
           icon={<HomeIcon />}
           isActive={activeLink === "/"}
         />
         <MenuItem
-          title="Change In Heat Demand"
+          title={isSidebarOpen ? "Change in Heat Demand" : ""}
           href="/demand"
           icon={<GlobeIcon />}
           isActive={activeLink === "/demand"}
         />
         <MenuItem
-          title="Energy Efficiency Costs"
+          title={isSidebarOpen ? "Energy Efficieny Costs" : ""}
           href="/costs"
           icon={<PoundIcon />}
           isActive={activeLink === "/costs"}
         />
         <MenuItem
-          title="Heat Consumption Profile"
+          title={isSidebarOpen ? "Heat Consumption Profile" : ""}
           href="/consumption"
           icon={<GraphDown />}
           isActive={activeLink === "/consumption"}
         />
       </nav>
-      <div className="flex items-center justify-center py-4">
-        <Link
-          className="mx-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
-          href="/help"
-        >
-          Help
-        </Link>
-        <Link
-          className="mx-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
-          href="/contact"
-        >
-          Contact
-        </Link>
-      </div>
+      {isSidebarOpen && (
+        <div className="flex items-center justify-center py-4">
+          <Link
+            href="/help"
+            className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+          >
+            Help
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
