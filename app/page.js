e8c8fc3c8c0416defad7d48d17911d7e9a851ec8
Antonio <derosaa@cardiff.ac.uk>
2023-10-31 13:50:45 +0000

    home page with relevant name, created boxes for information

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import { Roboto_Flex } from 'next/font/google';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <div className='h-screen flex flex-col items-center justify-center'>
+        <div className='text-6xl mb-4'>HEATWISE</div>
+        <div className='flex w-full'>
+          <div class='grow w-1/2 h-48 mx-4 border-4 mb-4'></div>
+          <div class='grow w-1/2 h-48 mx-4 border-4 mb-4'></div>
+        </div>
+      </div>
+    </>
   );
 }
