90baca6a00d2d6ee24f5981d0fef38802a4419d9
Sanil Jalan <JalanS1@cardiff.ac.uk>
2023-10-31 18:04:56 +0000

    fix invalid DOM property errors

--- app/components/icons/Question.js
+++ app/components/icons/Question.js
@@ -1,6 +1,6 @@
 const QuestionMark = () => (
   <svg
-    class='w-6 h-6 text-gray-800 dark:text-white'
+    className='w-6 h-6 text-gray-800 dark:text-white'
     aria-hidden='true'
     xmlns='http://www.w3.org/2000/svg'
     fill='none'
@@ -8,9 +8,9 @@ const QuestionMark = () => (
   >
     <path
       stroke='currentColor'
-      stroke-linecap='round'
-      stroke-linejoin='round'
-      stroke-width='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='1.5'
       d='M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
     />
   </svg>
