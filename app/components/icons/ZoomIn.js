b193c4a1985d550290105d8e8d2b2effc7b807cc
Toby Nott <tobynott80@gmail.com>
2023-11-04 17:12:44 +0000

    added icons

---
+++ app/components/icons/ZoomIn.js
@@ -0,0 +1,21 @@
+// Icons free and open source from https://flowbite.com/icons/
+
+const ZoomIn = () => (
+  <svg
+    class='w-4 h-4 text-gray-800 dark:text-white'
+    aria-hidden='true'
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 20 20'
+  >
+    <path
+      stroke='currentColor'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      stroke-width='2'
+      d='m19 19-4-4M5 8h6m-3 3V5m7 3A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
+    />
+  </svg>
+);
+
+export default ZoomIn;
---
+++ app/components/icons/ZoomOut.js
@@ -0,0 +1,21 @@
+// Icons free and open source from https://flowbite.com/icons/
+
+const ZoomOut = () => (
+  <svg
+    class='w-4 h-4 text-gray-800 dark:text-white'
+    aria-hidden='true'
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 20 20'
+  >
+    <path
+      stroke='currentColor'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      stroke-width='2'
+      d='m19 19-4-4M5 8h6m4 0A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
+    />
+  </svg>
+);
+
+export default ZoomOut;
