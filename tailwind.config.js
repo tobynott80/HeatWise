e728d1de3ddac1a6bf37671c26d3e22db2656923
Toby Nott <tobynott80@gmail.com>
2023-11-05 15:32:22 +0000

    added animation for hints

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      animation: {
+        'spin-once': 'spin 0.5s linear 2s'
       }
     }
   },
