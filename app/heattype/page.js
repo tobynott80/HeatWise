d5f542300f636a4c37e862ca22ac305a3dd23c64
George Hampton <ghampton4727@gmail.com>
2023-11-14 17:29:26 +0000

    Added interactive 3d map that posts example api data
    Need to get the data working with our own data and plot it effectively

--- app/heattype/page.js
+++ app/heattype/page.js
@@ -1,122 +1,36 @@
 'use client'
-import React, {useState} from 'react';
-import {createRoot} from 'react-dom/client';
-import {Map} from 'react-map-gl';
-import maplibregl from 'maplibre-gl';
-import DeckGL from '@deck.gl/react';
-import {GeoJsonLayer, PolygonLayer} from '@deck.gl/layers';
-import {LightingEffect, AmbientLight, _SunLight as SunLight} from '@deck.gl/core';
-import {scaleThreshold} from 'd3-scale';
+import React, {useState,useEffect} from 'react'
+import LocationAggregatorMap from "@/components/Map";
 
-// Source data GeoJSON
-const DATA_URL =
-    '/eng_wal_lad.geojson'; // eslint-disable-line
+const HomePage = () => {
+    const [details, setDetails] = useState([]);
+    const [coordinates, setCoordinates] = useState([]);
 
-export const COLOR_SCALE = scaleThreshold()
-    .domain([-0.6, -0.45, -0.3, -0.15, 0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2])
-    .range([
-        [65, 182, 196],
-        [127, 205, 187],
-        [199, 233, 180],
-        [237, 248, 177],
-        // zero
-        [255, 255, 204],
-        [255, 237, 160],
-        [254, 217, 118],
-        [254, 178, 76],
-        [253, 141, 60],
-        [252, 78, 42],
-        [227, 26, 28],
-        [189, 0, 38],
-        [128, 0, 38]
-    ]);
+    useEffect(() => {
+        const getData = async () => {
+            const response = await fetch(
+                "https://api-shipx-pl.easypack24.net/v1/points?per_page=28000"
+            );
 
-const INITIAL_VIEW_STATE = {
-    latitude: 51.9837,
-    longitude: -3.0281,
-    zoom: 6,
-    maxZoom: 16,
-    pitch: 45,
-    bearing: 0
-};
+            const data = await response.json();
+            setDetails(data.items);
 
-const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
-
-const ambientLight = new AmbientLight({
-    color: [255, 255, 255],
-    intensity: 1.0
-});
-
-const dirLight = new SunLight({
-    timestamp: Date.UTC(2019, 7, 1, 22),
-    color: [255, 255, 255],
-    intensity: 1.0,
-    _shadow: true
-});
-
-const landCover = [
-    [
-        [-123.0, 49.196],
-        [-123.0, 49.324],
-        [-123.306, 49.324],
-        [-123.306, 49.196]
-    ]
-];
-
-function getTooltip({object}) {
-    return (
-        object && {
-            html: `\
-  <div><b>Average Property Value</b></div>
-  <div>${object.properties.valuePerParcel} / parcel</div>
-  <div>${object.properties.valuePerSqm} / m<sup>2</sup></div>
-  <div><b>Growth</b></div>
-  <div>${Math.round(object.properties.growth * 100)}%</div>
-  `
-        }
-    );
-}
-
-export default function App({data = DATA_URL, mapStyle = MAP_STYLE}) {
-    const [effects] = useState(() => {
-        const lightingEffect = new LightingEffect({ambientLight, dirLight});
-        lightingEffect.shadowColor = [0, 0, 0, 0.5];
-        return [lightingEffect];
-    });
-
-    const layers = [
-        // only needed when using shadows - a plane for shadows to drop on
-        new PolygonLayer({
-            id: 'ground',
-            data: landCover,
-            stroked: false,
-            getPolygon: f => f,
-            getFillColor: [0, 0, 0, 0]
-        }),
-        new GeoJsonLayer({
-            id: 'geojson',
-            data,
-            opacity: 0.8,
-            stroked: false,
-            filled: true,
-            extruded: true,
-            wireframe: true,
-            getElevation: f => Math.sqrt(f.properties.valuePerSqm) * 10,
-            getFillColor: f => COLOR_SCALE(f.properties.growth),
-            getLineColor: [255, 255, 255],
-            pickable: true
-        })
-    ];
+            // Create an array of geo coordinates pairs
+            const coords = data.items.map((item) => [
+                item.location.longitude,
+                item.location.latitude,
+            ]);
+            setCoordinates(coords);
+        };
+        getData();
+    }, []);
+    console.log(coordinates);
 
     return (
-        <DeckGL
-            layers={layers}
-            effects={effects}
-            initialViewState={INITIAL_VIEW_STATE}
-            controller={true}
-            getTooltip={getTooltip}
-        >
-            <Map reuseMaps mapLib={maplibregl} mapStyle={mapStyle} preventStyleDiffing={true} />
-        </DeckGL>
-    );
+        <div className="relative min-h-screen">
+            <LocationAggregatorMap data={coordinates} />
+        </div>
+    )
 }
+
+export default HomePage
---
+++ components/Map.jsx
@@ -0,0 +1,117 @@
+"use client"
+
+import React, { useState } from 'react';
+
+import Map from 'react-map-gl'
+import { HexagonLayer } from '@deck.gl/aggregation-layers'
+import DeckGL from '@deck.gl/react'
+import "mapbox-gl/dist/mapbox-gl.css"
+
+//import map config
+import {
+    lightingEffect,
+    material,
+    INITIAL_VIEW_STATE,
+    colorRange,
+} from "@/components/mapconfig";
+
+const LocationAggregatorMap = ({
+                                   upperPercentile = 100,
+                                   coverage = 1,
+                                   data,
+                               }) => {
+
+
+    const [radius, setRadius] = useState(1000);
+
+    const handleRadiusChange = (e) => {
+        console.log(e.target.value);
+        setRadius(e.target.value);
+    };
+
+    // creating tooltip
+    function getTooltip({ object }) {
+        if (!object) {
+            return null;
+        }
+        const lat = object.position[1];
+        const lng = object.position[0];
+        const count = object.points.length;
+
+        return `\
+        latitude: ${Number.isFinite(lat) ? lat.toFixed(6) : ""}
+        longitude: ${Number.isFinite(lng) ? lng.toFixed(6) : ""}
+        ${count} locations here`;
+    }
+
+    const layers = [
+        new HexagonLayer({
+            id: "heatmap",
+            colorRange,
+            coverage,
+            data,
+            elevationRange: [0, 3000],
+            elevationScale: data && data.length ? 50 : 0,
+            extruded: true,
+            getPosition: (d) => d,
+            pickable: true,
+            radius,
+            upperPercentile,
+            material,
+
+            transitions: {
+                elevationScale: 3000,
+            },
+        }),
+    ];
+
+    return (
+        <div>
+            <DeckGL
+                layers={layers}
+                effects={[lightingEffect]}
+                initialViewState={INITIAL_VIEW_STATE}
+                controller={true}
+                getTooltip={getTooltip}
+            >
+                <Map
+                    className=""
+                    controller={true}
+                    mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+                    mapStyle="mapbox://styles/petherem/cl2hdvc6r003114n2jgmmdr24"
+                ></Map>
+                {/* FLOATING CONTROLLER */}
+
+                <div className="absolute bg-slate-900 text-white min-h-[200px] h-auto w-[250px] top-10 left-5 rounded-lg p-4 text-sm">
+                    <div className="flex flex-col">
+                        <h2 className="font-bold text-xl uppercase mb-1">Map controller</h2>
+                        <h2 className="font-bold text-md uppercase mb-4">INPOST LOCS</h2>
+                        <input
+                            name="radius"
+                            className="w-fit py-2"
+                            type="range"
+                            value={radius}
+                            min={500}
+                            step={50}
+                            max={10000}
+                            onChange={handleRadiusChange}
+                        />
+                        <label htmlFor="radius">
+                            Radius -{" "}
+                            <span className="bg-indigo-500 font-bold text-white px-2 py-1 rounded-lg">
+                {radius}
+              </span>{" "}
+                            meters
+                        </label>
+                        <p>
+                            {" "}
+                            <span className="font-bold">{data.length}</span> Locations
+                        </p>
+                    </div>
+                </div>
+            </DeckGL>
+        </div>
+    );
+};
+
+export default LocationAggregatorMap
