a606856496dd04d4f693b7571b6e039522df3023
Sanil Jalan <JalanS1@cardiff.ac.uk>
2023-11-28 10:21:41 +0000

    init page

---
+++ app/costs/page.js
@@ -0,0 +1,17 @@
+export default function Costs() {
+  return (
+    <div className='px-2 pt-3'>
+      <div className='flex flex-col h-full justify-center items-center'>
+        <div className='grid grid-cols-2 mb-4 w-full flex-nowrap items-center'>
+            hello
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*
+1. Setup db tables + import
+2. Draw pie charts
+3. List authorities
+*/
