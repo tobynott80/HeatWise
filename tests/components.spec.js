5377257b669da3367c44cc734cfc1a1c696db41e
Tobiah Nott <notttj@cardiff.ac.uk>
2023-12-16 00:19:12 +0000

    fixed navbar tests

--- tests/components.spec.js
+++ tests/components.spec.js
@@ -12,17 +12,24 @@ test('Navbar List Items', async ({ page }) => {
   await expect(page.locator('text=Heat Consumption Profile')).toBeVisible();
 });
 
-test('Navbar navigation redirect', async ({ page }) => {
+test('Navbar demand navigation redirect', async ({ page }) => {
   await page.goto('/');
   await page.click('text=Change in Heat Demand');
-  await page.waitForNavigation();
+  await page.waitForURL("/demand");
   await expect(page).toHaveURL('/demand');
-
+});
+test('Navbar efficiency costs navigation redirect', async ({ page }) => {
+  await page.goto('/');
   await page.click('text=Energy Efficiency Costs');
-  await page.waitForNavigation();
+  await page.waitForURL("/costs");
   await expect(page).toHaveURL('/costs');
+});
 
+test('Navbar heat consumption navigation redirect', async ({ page }) => {
+  await page.goto('/');
   await page.click('text=Heat Consumption Profile');
-  await page.waitForNavigation();
+  await page.waitForURL("/consumption");
   await expect(page).toHaveURL('/consumption');
 });
+
+
